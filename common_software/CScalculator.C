f2d4092b11d03fc410fc836661a1af24b8856370
Bruno Strandberg <b.strandberg@nikhef.nl>
2018-03-07 16:27:25 +0100

    Better naming in CScalculator.

--- common_software/CScalculator.C
+++ common_software/CScalculator.C
@@ -12,9 +12,9 @@ CScalculator::CScalculator(TString xsecfile) {
 
   //init the maps that are used throughout the code
 
-  nu_types.insert( pair<Int_t, TString> {0, "e"}   );
-  nu_types.insert( pair<Int_t, TString> {1, "mu"}  );
-  nu_types.insert( pair<Int_t, TString> {2, "tau"} );
+  nu_flavs.insert( pair<Int_t, TString> {0, "e"}   );
+  nu_flavs.insert( pair<Int_t, TString> {1, "mu"}  );
+  nu_flavs.insert( pair<Int_t, TString> {2, "tau"} );
 
   int_types.insert( pair<Int_t, TString> {0, "_nc"} );
   int_types.insert( pair<Int_t, TString> {1, "_cc"} );
@@ -55,12 +55,12 @@ CScalculator::~CScalculator() {
 /**
  * This function selects the interaction for which the fuction CalculateCS(E) returns the xsec.
  *
- * \param  nu_type       Neutrino type
+ * \param  nu_flavor     Neutrino flavor
  * \param  is_cc         Interaction type (0 - nc, 1 - cc)
  * \param  is_nubar      0 - particle, 1 - antiparticle.
  *
  */
-void CScalculator::SelectInteraction(Int_t nu_type, Bool_t is_cc, Bool_t is_nubar) {
+void CScalculator::SelectInteraction(Int_t nu_flavor, Bool_t is_cc, Bool_t is_nubar) {
 
   //--------------------------------------------------------------
   //check that the interaction type is supported (in the maps)
@@ -68,8 +68,8 @@ void CScalculator::SelectInteraction(Int_t nu_type, Bool_t is_cc, Bool_t is_nuba
 
   Bool_t supported = true;
 
-  if ( nu_types.find(nu_type) == nu_types.end() ) {
-    cout << "ERROR! CScalculator::SelectInteraction() neutrino type " << nu_type 
+  if ( nu_flavs.find(nu_flavor) == nu_flavs.end() ) {
+    cout << "ERROR! CScalculator::SelectInteraction() neutrino flavor " << nu_flavor 
 	 << " not supported" << endl;
     supported = false;
   }
@@ -93,7 +93,7 @@ void CScalculator::SelectInteraction(Int_t nu_type, Bool_t is_cc, Bool_t is_nuba
 
   if (supported) {
 
-    TString searchstr = CreateString(nu_type, is_cc, is_nubar);
+    TString searchstr = CreateString(nu_flavor, is_cc, is_nubar);
     f_g_H1  = fGraphs[searchstr][0];
     f_g_O16 = fGraphs[searchstr][1];
 
@@ -143,20 +143,20 @@ Bool_t CScalculator::InitGraphs(TString xsecfile) {
   if ( !f->IsOpen() ) return false;
 
   //loop over maps, use the CreateString() function to create a lookup string
-  //for each combination of nu_type, int_type, p_type. Clone the xsec graphs to the map.
+  //for each combination of nu_flavor, int_type, p_type. Clone the xsec graphs to the map.
   
-  for (auto& nutype: nu_types) {
+  for (auto& nuflav: nu_flavs) {
     for (auto& inttype: int_types) {
       for (auto& ptype: p_types) {
 
-	TString gname1 = "nu_" + nutype.second + ptype.second + "_H1/tot" + inttype.second;
-	TString gname2 = "nu_" + nutype.second + ptype.second + "_O16/tot" + inttype.second;
+	TString gname1 = "nu_" + nuflav.second + ptype.second + "_H1/tot" + inttype.second;
+	TString gname2 = "nu_" + nuflav.second + ptype.second + "_O16/tot" + inttype.second;
 	
 	vector<TGraph*> graphs;
 	graphs.push_back( (TGraph*)( f->Get(gname1)->Clone() ) );
 	graphs.push_back( (TGraph*)( f->Get(gname2)->Clone() ) );
 	
-	TString namestr = CreateString( nutype.first, (Bool_t)inttype.first, (Bool_t)ptype.first );
+	TString namestr = CreateString( nuflav.first, (Bool_t)inttype.first, (Bool_t)ptype.first );
 	
 	fGraphs.insert( pair<TString, vector<TGraph*> > {namestr, graphs} );
 	
@@ -176,13 +176,13 @@ Bool_t CScalculator::InitGraphs(TString xsecfile) {
 /**
  * This function creates a string for searching/populating the map with xsec graphs.
  *
- * \param  nu_type       Neutrino type
+ * \param  nu_flavor     Neutrino flavor
  * \param  is_cc         Interaction type (0 - nc, 1 - cc)
  * \param  is_nubar      0 - particle, 1 - antiparticle.
  * \return               A search string for the fGraphs map.
  */
-TString CScalculator::CreateString(Int_t nu_type, Bool_t is_cc, Bool_t is_nubar) {
+TString CScalculator::CreateString(Int_t nu_flavor, Bool_t is_cc, Bool_t is_nubar) {
 
-  return nu_types[nu_type] + p_types[(Int_t)is_nubar] + int_types[(Int_t)is_cc];
+  return nu_flavs[nu_flavor] + p_types[(Int_t)is_nubar] + int_types[(Int_t)is_cc];
 
 }
--- common_software/CScalculator.h
+++ common_software/CScalculator.h
@@ -24,14 +24,14 @@ class CScalculator {
   CScalculator(TString xsecfile="../data/cross_sections_gSeaGen_v4r1/xsec.root");
   ~CScalculator();
 
-  void     SelectInteraction(Int_t nu_type, Bool_t is_cc, Bool_t is_nubar);
+  void     SelectInteraction(Int_t nu_flavor, Bool_t is_cc, Bool_t is_nubar);
   Double_t CalculateCS(Double_t E);
 
  private:
   Bool_t   InitGraphs(TString xsecfile);
-  TString  CreateString(Int_t nu_type, Bool_t is_cc, Bool_t is_nubar);
+  TString  CreateString(Int_t nu_flavor, Bool_t is_cc, Bool_t is_nubar);
 
-  map < Int_t, TString > nu_types;    //map with e, mu, tau
+  map < Int_t, TString > nu_flavs;    //map with e, mu, tau
   map < Int_t, TString > int_types;   //map with nc, cc
   map < Int_t, TString > p_types;     //map with "", "bar"
 
