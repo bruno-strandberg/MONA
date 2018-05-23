a0da41a370d527476084b2d780de131f4c2429a9
Bruno Strandberg <b.strandberg@nikhef.nl>
2018-05-23 14:57:10 +0200

    Update to sampler caller macro and evtid test script.

--- evt_sampler/sampler_caller.py
+++ evt_sampler/sampler_caller.py
@@ -3,7 +3,7 @@
 This script can be used to call the GSGSampler.C macro and execute it on the farm.
  
 Usage:
-    sampler_caller -f FLAVOR... -i INTERACTION... -a FLUX_LIST [-n SAMPLES]
+    sampler_caller -f FLAVOR... -i INTERACTION... -a FLUX_LIST [-n SAMPLES] [--execute]
     sampler_caller -h                                                                     
 
 Option:
@@ -12,6 +12,7 @@ Option:
     -a FLUX_LIST      Text file that lists the FluxChain.C output files input to GSGSampler.
                       flux_caller.py outputs such a file.
     -n SAMPLES        Samples per flux file [default: 5]
+    --execute         To send to farm, otherwise only scripts created
     -h --help         Show this screen
 
 """
@@ -41,26 +42,28 @@ def main(args):
             # make tmp directory for logfiles etc
             os.system("mkdir -p tmp/")
 
-            # create the list of gsg files
-            gsg_flist = "tmp/{0}_{1}_gsg_flist.dat".format( flav, inter )
-            os.system( "ls {0}*{1}*{2}* > {3}".format(gsg_dir, flav, inter, gsg_flist) )
-
-            # create the command to execute GSGSampler
-            cmd  = "root -b -q 'GSGSampler.C+("
-            cmd += '"' + args['-a'] + '", '  # flux file list
-            cmd += '"' + gsg_flist + '", '   # gseagen file list
-            cmd += str(int(f)) + ", "        # neutrino flavor
-            cmd += str(int(i)) + ", "        # neutrino interaction
-            cmd += str(int(args['-n']))      # number of samples per flux file
-            cmd += ")'"
-
-            # create a bash script than can be sent to farm
+            # get some necessary environment variables
             cwd  = os.getcwd()
             jpp  = os.environ['JPP_DIR']
             root = os.environ['ROOTSYS']
             nmh  = os.environ['NMHDIR']
 
-            script_name = "tmp/farm_job_{0}_{1}.sh".format(flav, inter)
+            # create the list of gsg files
+            gsg_flist = "{0}/tmp/{1}_{2}_gsg_flist.dat".format( cwd, flav, inter )
+            flux_file_list = "{0}/{1}".format(cwd, args['-a'])
+            os.system( "ls {0}*{1}*{2}* > {3}".format(gsg_dir, flav, inter, gsg_flist) )
+
+            # create the command to execute GSGSampler
+            cmd  = "root -b -q 'GSGSampler.C+("
+            cmd += '"' + flux_file_list + '", '  # flux file list
+            cmd += '"' + gsg_flist + '", '       # gseagen file list
+            cmd += str(int(f)) + ", "            # neutrino flavor
+            cmd += str(int(i)) + ", "            # neutrino interaction
+            cmd += str(int(args['-n']))          # number of samples per flux file
+            cmd += ")'"
+
+            # create a bash script than can be sent to farm
+            script_name = "{0}/tmp/farm_job_{1}_{2}.sh".format(cwd, flav, inter)
             script_file = open(script_name, 'w')
 
             script_file.write('#!/bin/bash\n\n')
@@ -71,7 +74,9 @@ def main(args):
 
             script_file.close()
 
-            #os.system("qsub -P P_km3net -l sps=1 -o {0} -e {0} {1}".format(cwd+"/tmp", script_name))
+            farm_cmd = "qsub -P P_km3net -l sps=1 -o {0} -e {0} {1}".format(cwd+"/tmp", script_name)
+            if args['--execute']:
+                os.system(farm_cmd)
 #===========================================================================
 
 if __name__ == "__main__":
--- tests/evtid_functionality.C
+++ tests/evtid_functionality.C
@@ -43,10 +43,11 @@ void evtid_functionality() {
   }
 
   cout << "******************************************************" << endl;
-  cout << "Find" << endl;
+  cout << "Search range" << endl;
   cout << "******************************************************" << endl;
 
-  std::pair<Int_t, Int_t> range = get_start_stop( evts, 5, 3 );
+  evtid test(5, 2, 3);
+  std::pair<Int_t, Int_t> range = get_start_stop( evts, test.run_nr, test.e_min );
 
   for (auto it = evts.begin() + range.first; it < evts.begin() + range.second; it++) {
     cout << it->run_nr << "\t" << it->evt_nr << "\t" << it->e_min << endl;
@@ -56,12 +57,27 @@ void evtid_functionality() {
     cout << i << "\t" << evts[i].run_nr << "\t" << evts[i].evt_nr << "\t" << evts[i].e_min << endl;
   }
 
-  evtid test(5, 2, 3);
+  cout << "******************************************************" << endl;
+  cout << "Find" << endl;
+  cout << "******************************************************" << endl;
+
+  TStopwatch find_time;
   if ( std::find(evts.begin() + range.first, evts.begin() + range.second, test) != ( evts.begin() + range.second) ) {
-    cout << "Found" << endl;
+    cout << "std:find: found" << endl;
   }
   else {
-    cout << "Not found" << endl; 
+    cout << "std:find: not found" << endl;
   }
+  cout << "std::find() time" << find_time.RealTime() << endl;
+  
+
+  TStopwatch bs_time;
+  if ( std::binary_search(evts.begin() + range.first, evts.begin() + range.second, test) ) {
+    cout << "Binary search: found" << endl;
+  }
+  else {
+    cout << "Binary search: not found" << endl; 
+  }
+  cout << "binary search time" << bs_time.RealTime() << endl;
 
 }
