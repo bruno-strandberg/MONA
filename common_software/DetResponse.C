71aefb485d80c2145fb5ed2eedad0e5a12f7fb5b
Bruno Strandberg <b.strandberg@nikhef.nl>
2018-10-22 10:37:44 +0200

    After further consideration, I believe this is the correct way to calculate the MC stat err in DetResponse.

--- common_software/DetResponse.C
+++ common_software/DetResponse.C
@@ -323,12 +323,16 @@ void DetResponse::Normalise() {
 	  // total number of simulated events in E_true, ct_true, by_true bin
 	  Double_t nsim  = hsim->GetBinContent(tb.fE_true_bin, tb.fCt_true_bin, tb.fBy_true_bin);
 
-	  // fW is at this point the number of events from true bin that entered reco bin; convert to weight
-	  tb.fW = tb.fW/nsim;
-
-	  // calculate the error of the weight from the relative poisson error of the true bin
-	  tb.fWE = tb.fW * TMath::Sqrt( nsim )/nsim;
-
+	  // number of events that entered from true bin to reco bin (stored in tb.fW at this point)
+	  Double_t nsel = tb.fW;
+	  
+	  // calculate the weight (fraction of simulated events in true bin that entered the reco bin)
+	  tb.fW = nsel/nsim;
+	  
+	  // calculate the weight error from error propagation for fW = nsel/nsim under the
+	  // assumption that nsel << nsim
+	  tb.fWE = TMath::Sqrt(nsel)/nsim;
+	  
 	  //------------------------------------------------------------
 	  // calculate the fraction the events from true bin make up from the reco bin
 	  // this is used in `DetResponse::DisplayResponse`, the sum over fFracReco for each reco bin = 1
