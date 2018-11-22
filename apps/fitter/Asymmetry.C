b2d1362252a937a2181b9a1fbab36483f15b4676
Bruno Strandberg <b.strandberg@nikhef.nl>
2018-11-22 16:36:44 +0100

    Updated the asymmetry code.

--- apps/fitter/Asymmetry.C
+++ apps/fitter/Asymmetry.C
@@ -9,13 +9,18 @@
 #include "RooRealVar.h"
 
 #include "TFile.h"
+#include "TGraph.h"
+
+#include <stdexcept>
 
 // jpp headers
 #include "Jeep/JParser.hh"
 #include "Jeep/JMessage.hh"
+#include "JTools/JRange.hh"
 
 using namespace RooFit;
 using namespace std;
+using namespace JTOOLS;
 
 int main(int argc, char **argv) {
 
@@ -23,60 +28,78 @@ int main(int argc, char **argv) {
   // parse command line arguments with Jpp
   //----------------------------------------------------------
   
-  string        simdata_file;
-  bool          refill_response;
-  string        outputfile;
-  string        effmh_elecCC;
-  string        effmh_muonCC;
-  string        effmh_tauCC;
-  string        effmh_elecNC;
+  TString        simdata_file;
+  bool           refill_response;
+  TString        outputfile;
+  JRange<double> MC_E_range;
+  Int_t          nebins;
+  Int_t          nctbins;
+  JRange<double> range_th23;
+  Int_t          nsteps;
+  TString        effmh_elecCC;
+  TString        effmh_muonCC;
+  TString        effmh_tauCC;
+  TString        effmh_elecNC;
   
   try {
 
     JParser<> zap("Program to calculate asymmetries with standard track-shower separation.");
 
     zap['s'] = make_field(simdata_file, "File with all summary data") =
-      (string)getenv("NMHDIR") + "/data/ORCA_MC_summary_all_10Apr2018.root";
+      (TString)getenv("NMHDIR") + "/data/ORCA_MC_summary_ORCA115_23x9m_ECAP0418.root";
 
     zap['r'] = make_field(refill_response, "Flag to request re-filling of the detector responses");
-    zap['o'] = make_field(outputfile, "File where output histograms are written") = "rootfiles/asymmetry.root";
+    zap['o'] = make_field(outputfile, "File where output histograms are written") = NMHUtils::Getcwd() + "/rootfiles/asymmetry.root";
+    zap['e'] = make_field(MC_E_range, "Energy range of the MC data") = JRange<double>(1, 100);
+    zap['E'] = make_field(nebins, "Number of energy bins in the energy range") = 40;
+    zap['C'] = make_field(nctbins, "Number of cos-theta bins in the range -1 to 1") = 80;
+    zap['a'] = make_field(range_th23, "sin^2 theta23 range") = JRange<double>(0.5, 0.5);
+    zap['n'] = make_field(nsteps, "Number of steps in sin^2 theta23 range") = 1;
 
     zap['w'] = make_field(effmh_elecCC, "Eff mass histograms for elec-CC") =
-      (string)getenv("NMHDIR") + "/data/eff_mass/EffMhists_elec_CC.root";
+      (TString)getenv("NMHDIR") + "/data/eff_mass/EffMhists_elec_CC.root";
 
     zap['x'] = make_field(effmh_muonCC, "Eff mass histograms for muon-CC") =
-      (string)getenv("NMHDIR") + "/data/eff_mass/EffMhists_muon_CC.root";
+      (TString)getenv("NMHDIR") + "/data/eff_mass/EffMhists_muon_CC.root";
 
     zap['y'] = make_field(effmh_tauCC , "Eff mass histograms for tau-CC") =
-      (string)getenv("NMHDIR") + "/data/eff_mass/EffMhists_tau_CC.root";
+      (TString)getenv("NMHDIR") + "/data/eff_mass/EffMhists_tau_CC.root";
 
     zap['z'] = make_field(effmh_elecNC, "Eff mass histograms for elec-NC") =
-      (string)getenv("NMHDIR") + "/data/eff_mass/EffMhists_elec_NC.root";    
+      (TString)getenv("NMHDIR") + "/data/eff_mass/EffMhists_elec_NC.root";    
     zap(argc, argv);
   }
   catch(const exception &error) {
     FATAL(error.what() << endl);
+  }
+
+  if ( range_th23.getLowerLimit() == range_th23.getUpperLimit() && nsteps != 1 ) {
+    throw std::invalid_argument("ERROR! requesting theta-23 steps in range width 0.");
+  }
+
+  if ( range_th23.getLowerLimit() != range_th23.getUpperLimit() && nsteps == 1 ) {
+    throw std::invalid_argument("ERROR! at least two steps are required in a range with width > 0.");
   }
   
   //----------------------------------------------------------
   // set up the detector responses
   //----------------------------------------------------------
   
-  Int_t fENB   =  40;
-  Int_t fEmin  =   1;
-  Int_t fEmax  = 100;
-  Int_t fCtNB  =  40;
-  Int_t fCtmin =  -1;
-  Int_t fCtmax =   1;
-  Int_t fByNB  =   1;
-  Int_t fBymin =   0;
-  Int_t fBymax =   1;
+  Int_t emin  = MC_E_range.getLowerLimit();
+  Int_t emax  = MC_E_range.getUpperLimit();
+  Int_t ctmin   = -1;
+  Int_t ctmax   =  1;
+
+  // for asym analysis always use 1 bjorken-y bin
+  Int_t nbybins =  1;
+  Int_t bymin   =  0;
+  Int_t bymax   =  1;
   
   DetResponse track_resp (DetResponse::track, "track_resp", 
-			  fENB, fEmin, fEmax, fCtNB, fCtmin, fCtmax, fByNB, fBymin, fBymax);
+			  nebins, emin, emax, nctbins, ctmin, ctmax, nbybins, bymin, bymax);
 
   DetResponse shower_resp(DetResponse::shower, "shower_resp", 
-			  fENB, fEmin, fEmax, fCtNB, fCtmin, fCtmax, fByNB, fBymin, fBymax);
+			  nebins, emin, emax, nctbins, ctmin, ctmax, nbybins, bymin, bymax);
 
   track_resp.AddCut( &SummaryEvent::Get_track_ql0       , std::greater<double>()   ,  0.5, true );
   track_resp.AddCut( &SummaryEvent::Get_track_ql1       , std::greater<double>()   ,  0.5, true );
@@ -90,8 +113,8 @@ int main(int argc, char **argv) {
   shower_resp.AddCut( &SummaryEvent::Get_RDF_muon_score , std::less_equal<double>(), 0.05, true );
   shower_resp.AddCut( &SummaryEvent::Get_RDF_noise_score, std::less_equal<double>(),  0.5, true );
 
-  TString track_resp_name  = "rootfiles/track_response.root";
-  TString shower_resp_name = "rootfiles/shower_response.root";
+  TString track_resp_name  = NMHUtils::Getcwd() + "/rootfiles/track_response.root";
+  TString shower_resp_name = NMHUtils::Getcwd() + "/rootfiles/shower_response.root";
   
   if ( !NMHUtils::FileExists(track_resp_name) || !NMHUtils::FileExists(shower_resp_name) || refill_response ) {
 
@@ -124,18 +147,17 @@ int main(int argc, char **argv) {
   cout << "NOTICE Asymmetry() Responses ready" << endl;
 
   //----------------------------------------------------------
-  // set up the PDFs
+  // set up the PDFs and static oscillation parameters
   //----------------------------------------------------------
   
   FitUtil *fitutil = new FitUtil(3, track_resp.GetHist3D(),
-  				 1, 100, -1, 0, 0, 1, effmh_elecCC, effmh_muonCC, effmh_tauCC, effmh_elecNC);
+  				 1, 100, -1, 1, 0, 1, effmh_elecCC, effmh_muonCC, effmh_tauCC, effmh_elecNC);
 
   FitPDF pdf_tracks("pdf_tracks", "pdf_tracks"   , fitutil, &track_resp);  
   FitPDF pdf_showers("pdf_showers", "pdf_showers", fitutil, &shower_resp);
 
   Double_t sinsqth12 = TMath::Power( TMath::Sin( 33.4 * TMath::Pi()/180. ), 2 );
   Double_t sinsqth13 = TMath::Power( TMath::Sin( 8.42 * TMath::Pi()/180. ), 2 );
-  Double_t sinsqth23 = TMath::Power( TMath::Sin( 45   * TMath::Pi()/180. ), 2 );
   Double_t dcp       = 0.;
   Double_t dm32      = 2.44e-3;
   Double_t dm21      = 7.53e-5;
@@ -143,53 +165,90 @@ int main(int argc, char **argv) {
 
   ( (RooRealVar*)fitutil->GetSet().find("SinsqTh12") )->setVal( sinsqth12 );
   ( (RooRealVar*)fitutil->GetSet().find("SinsqTh13") )->setVal( sinsqth13 );
-  ( (RooRealVar*)fitutil->GetSet().find("SinsqTh23") )->setVal( sinsqth23 );
   ( (RooRealVar*)fitutil->GetSet().find("dcp") )->setVal( dcp );
   ( (RooRealVar*)fitutil->GetSet().find("Dm21") )->setVal( dm21 );
 
-  //----------------------------------------------------------
-  // set normal hierarchy
-  //----------------------------------------------------------
-  fitutil->SetNOlims();
-  Double_t dm31 = DM + 0.5*dm21;
-  ( (RooRealVar*)fitutil->GetSet().find("Dm31") )->setVal( dm31 );
-  
-  TH2D *tracks_NH  = (TH2D*)pdf_tracks.GetExpValHist()->Project3D("yx");
-  TH2D *showers_NH = (TH2D*)pdf_showers.GetExpValHist()->Project3D("yx");
-  tracks_NH->SetNameTitle("tracks_NH","tracks_NH");
-  showers_NH->SetNameTitle("showers_NH","showers_NH");
+  // deconstrain th23 and dm31
+  ( (RooRealVar*)fitutil->GetSet().find("Dm31") )->setMin( -1 );
+  ( (RooRealVar*)fitutil->GetSet().find("Dm31") )->setMax(  1 );
+  ( (RooRealVar*)fitutil->GetSet().find("SinsqTh23") )->setMin( -1 );
+  ( (RooRealVar*)fitutil->GetSet().find("SinsqTh23") )->setMax(  1 );
 
   //----------------------------------------------------------
-  // set inverted hierarchy
+  // loop over theta23 range and calculate asymmetries
   //----------------------------------------------------------
-  dm31 = -DM + 0.5*dm21; //IH
-  fitutil->SetIOlims();
-  ( (RooRealVar*)fitutil->GetSet().find("Dm31") )->setVal( dm31 );
-  
-  TH2D *tracks_IH  = (TH2D*)pdf_tracks.GetExpValHist()->Project3D("yx");
-  TH2D *showers_IH = (TH2D*)pdf_showers.GetExpValHist()->Project3D("yx");
-  tracks_IH->SetNameTitle("tracks_IH","tracks_IH");
-  showers_IH->SetNameTitle("showers_IH","showers_IH");
 
-  //----------------------------------------------------------
-  // asymmetry calculation
-  //----------------------------------------------------------
-  
-  auto trackasym  = NMHUtils::Asymmetry( tracks_NH, tracks_IH, "asym_tracks", 1,100,-1,0);
-  auto showerasym = NMHUtils::Asymmetry( showers_NH, showers_IH, "asym_showers", 1,100,-1,0);
+  TGraph asym_vs_th23;
+  vector<TH2D*> hlist;
+  Double_t step_size = 0;
 
-  cout << "NOTICE Asymmetry() track  asym: " << std::get<1>(trackasym) << endl;
-  cout << "NOTICE Asymmetry() shower asym: " << std::get<1>(showerasym) << endl;
+  if (nsteps > 1) {
+    step_size = ( range_th23.getUpperLimit() - range_th23.getLowerLimit() ) / (nsteps-1);
+  }
+
+  for (Int_t step = 0; step < nsteps; step++) {
+
+    Double_t sinsqth23 = range_th23.getLowerLimit() + step * step_size;
+    ( (RooRealVar*)fitutil->GetSet().find("SinsqTh23") )->setVal( sinsqth23 );
+    TString th23str = (TString)to_string(sinsqth23*1e3);
+    TString prefix = "th23x1e3_" + (TString)th23str(0,3) + "_";
+
+    //----------------------------------------------------------
+    // set normal hierarchy
+    //----------------------------------------------------------
+    Double_t dm31 = DM + 0.5*dm21;
+    ( (RooRealVar*)fitutil->GetSet().find("Dm31") )->setVal( dm31 );
+    
+    TH2D *tracks_NH  = (TH2D*)pdf_tracks.GetExpValHist()->Project3D("yx");
+    TH2D *showers_NH = (TH2D*)pdf_showers.GetExpValHist()->Project3D("yx");
+    tracks_NH->SetNameTitle(prefix+"tracks_NH",prefix+"tracks_NH");
+    showers_NH->SetNameTitle(prefix+"showers_NH",prefix+"showers_NH");
+    hlist.push_back(tracks_NH);
+    hlist.push_back(showers_NH);
+
+    //----------------------------------------------------------
+    // set inverted hierarchy
+    //----------------------------------------------------------
+    dm31 = -DM + 0.5*dm21; //IH
+    ( (RooRealVar*)fitutil->GetSet().find("Dm31") )->setVal( dm31 );
   
+    TH2D *tracks_IH  = (TH2D*)pdf_tracks.GetExpValHist()->Project3D("yx");
+    TH2D *showers_IH = (TH2D*)pdf_showers.GetExpValHist()->Project3D("yx");
+    tracks_IH->SetNameTitle(prefix+"tracks_IH",prefix+"tracks_IH");
+    showers_IH->SetNameTitle(prefix+"showers_IH",prefix+"showers_IH");
+    hlist.push_back(tracks_IH);
+    hlist.push_back(showers_IH);
+
+    //----------------------------------------------------------
+    // asymmetry calculation
+    //----------------------------------------------------------
+  
+    auto trackasym  = NMHUtils::Asymmetry( tracks_NH, tracks_IH, prefix+"asym_tracks",    -1,1e3,-1,0);
+    auto showerasym = NMHUtils::Asymmetry( showers_NH, showers_IH, prefix+"asym_showers", -1,1e3,-1,0);
+
+    Double_t th23 = TMath::ASin( TMath::Sqrt(sinsqth23) ) * 180./TMath::Pi();
+    cout << "NOTICE Asymmetry() at th23 = " << th23 << ", tracks: " << std::get<1>(trackasym) << endl;
+    cout << "NOTICE Asymmetry() at th23 = " << th23 << ", showers: " << std::get<1>(showerasym) << endl;
+    hlist.push_back( std::get<0>(trackasym)  );
+    hlist.push_back( std::get<0>(showerasym) );    
+
+    Double_t combA = TMath::Sqrt( TMath::Power( std::get<1>(trackasym), 2 ) + 
+				  TMath::Power( std::get<1>(showerasym), 2 ) );
+
+    asym_vs_th23.SetPoint( asym_vs_th23.GetN(), th23, combA );    
+
+  }
+
+  asym_vs_th23.SetMarkerStyle(20);
+  asym_vs_th23.SetMarkerColor(kBlue);
+  asym_vs_th23.SetLineWidth(2);
+  asym_vs_th23.SetLineColor(kBlue);
+  asym_vs_th23.SetTitle("Asymmetry vs theta-23");
+
   // write stuff out
   TFile fout( (TString)outputfile, "RECREATE");
-  tracks_NH->Write("tracks_NH");
-  showers_NH->Write("showers_NH");
-  tracks_IH->Write("tracks_IH");
-  showers_IH->Write("showers_IH");
-  std::get<0>(trackasym)->Write();
-  std::get<0>(showerasym)->Write();
+  for (auto h: hlist) h->Write();
+  asym_vs_th23.Write("asym_vs_th23");
   fout.Close();
 
 }
-
