b9f6a4a1d606c2e2adb6f9cfe6a9af34b3eac1f9
Bruno Strandberg <b.strandberg@nikhef.nl>
2018-12-10 11:40:37 +0100

    Update to setenv.sh

--- setenv.sh
+++ setenv.sh
@@ -9,49 +9,47 @@ if [ -z $JPP_DIR ] || [ -z $AADIR ]; then
     return 1
 fi
 
-oscprob=''
-
-while getopts 'o:' flag; do
-  case "${flag}" in
-    o) oscprob="${OPTARG}" ;;
-    *) error "Unexpected option ${flag}" ;;
-  esac
-done
-
-oscprob="$(readlink -e $oscprob)"
+if [ -z $OSCPROBDIR ]; then
+    echo "ERROR! NMH/setenv.sh environment variable OSCPROBDIR is not set"
+    return 1
+fi
 
 #==============================================================
 # NMHDIR is used in the code to avoid the use of relative path. NMHDIR extraction trick from https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within and Swim. Directories with shares libs are added to LD_LIBRARY_PATH. CPATH allows one to do anywhere eg "#include "AtmFlux.h"
 #==============================================================
 
-export NMHDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-export OSCPROBDIR="$oscprob"
+export NMHDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && readlink -e ./ )"
 
 # only add to path if the directory is not already included
 
 if ! echo $LD_LIBRARY_PATH | grep -q $NMHDIR/common_software; then
     export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:$NMHDIR/common_software
+    echo "NMH/setenv.sh::added $NMHDIR/common_software to LD_LIBRARY_PATH"
 fi
 
 if ! echo $LD_LIBRARY_PATH | grep -q $NMHDIR/fitter_software; then
     export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:$NMHDIR/fitter_software
+    echo "NMH/setenv.sh::added $NMHDIR/fitter_software to LD_LIBRARY_PATH" 
 fi
 
 if ! echo $LD_LIBRARY_PATH | grep -q $OSCPROBDIR; then
     export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:$OSCPROBDIR
+    echo "NMH/setenv.sh::added $OSCPROBDIR to LD_LIBRARY_PATH" 
 fi
 
 if ! echo $CPATH | grep -q $NMHDIR/common_software; then
     export CPATH=${CPATH}:$NMHDIR/common_software
+    echo "NMH/setenv.sh::added $NMHDIR/common_software to CPATH" 
 fi
 
 if ! echo $CPATH | grep -q $NMHDIR/fitter_software; then
     export CPATH=${CPATH}:$NMHDIR/fitter_software
+    echo "NMH/setenv.sh::added $NMHDIR/fitter_software to CPATH" 
 fi
 
 if ! echo $CPATH | grep -q $OSCPROBDIR; then
     export CPATH=${CPATH}:$OSCPROBDIR
+    echo "NMH/setenv.sh::added $OSCPROBDIR to CPATH" 
 fi
 
-echo "NMH/setenv.sh::OscProb directory --> $oscprob" 
 echo "NMH/setenv.sh::finished" 
