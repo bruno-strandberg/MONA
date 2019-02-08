862120bf9c8c28ed4a1f48f52a693f91caa0ca82
Lodewijk Nauta <lnauta@outlook.com>
2019-02-08 11:29:15 +0100

    Add python plot scripts for sensitivity on th23-range output

---
+++ macros/asimov_fits/plot_th23_range.py
@@ -0,0 +1,22 @@
+#!/bin/env/python
+
+import numpy as np
+import pandas as pd
+import sys
+
+import matplotlib.pyplot as plt
+
+filefolder = "./"
+filename = sys.argv[1] if (len(sys.argv) > 1) else "AsimovFitTh23Range.txt"
+
+df = pd.read_csv(filefolder + filename)
+
+df['chi2_no'] = np.sqrt(df['n_chi2tr_no']**2 + df['n_chi2sh_no']**2)
+df['chi2_io'] = np.sqrt(df['n_chi2tr_io']**2 + df['n_chi2sh_io']**2)
+
+df.plot(x="th23", y=["n_chi2tr_no", "n_chi2sh_no", "chi2_no", "n_chi2tr_io", "n_chi2sh_io", "chi2_io"])
+plt.xlabel(r"$\theta_{23}$")
+plt.ylabel("Sensitivity")
+plt.grid()
+plt.show()
+
