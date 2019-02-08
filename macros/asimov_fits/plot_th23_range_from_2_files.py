6bbe51e3f13bf5b3a509792e67c0ef2fc0e40a82
Lodewijk Nauta <lnauta@outlook.com>
2019-02-08 16:28:32 +0100

    Add dashed lines to sensitivity plot

--- macros/asimov_fits/plot_th23_range_from_2_files.py
+++ macros/asimov_fits/plot_th23_range_from_2_files.py
@@ -18,7 +18,8 @@ df = pd.merge(df1, df2, left_on="th23", right_on="th23")
 df['chi2_no'] = np.sqrt(df['n_chi2tr_no']**2 + df['n_chi2sh_no']**2)
 df['chi2_io'] = np.sqrt(df['n_chi2tr_io']**2 + df['n_chi2sh_io']**2)
 
-df.plot(x="th23", y=["n_chi2tr_no", "n_chi2sh_no", "chi2_no", "n_chi2tr_io", "n_chi2sh_io", "chi2_io"])
+df.plot(x="th23", y=["n_chi2tr_no", "n_chi2sh_no", "chi2_no", "n_chi2tr_io", "n_chi2sh_io", "chi2_io"],
+        style=['-','-','-','--','--','--'])
 plt.xlabel(r"$\theta_{23}$")
 plt.ylabel("Sensitivity")
 plt.grid()
