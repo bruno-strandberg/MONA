c3939dcc82090bfb540ec9a32b4beb68165539af
Lodewijk Nauta <lnauta@outlook.com>
2019-04-25 13:03:12 +0200

    Add root file writer, chi2fit instead of ml

--- macros/asimov_fits/AsimovFitNO.C
+++ macros/asimov_fits/AsimovFitNO.C
@@ -233,13 +233,13 @@ void AsimovFitNO() {
   // Fit in both quadrants to find the real minimum of Th23.
   fitutil->SetNOcentvals();
   fitutil->GetVar("SinsqTh23")->setVal(0.4);
-  RooFitResult *fitres_1q = simPdf.fitTo( data_hists, Save(), Range("fitRangeE"), Range("firstq"), 
+  RooFitResult *fitres_1q = simPdf.chi2FitTo( data_hists, Save(), Range("fitRangeE"), Range("firstq"), 
                                           SplitRange(isRanged), SplitRange(kFALSE), DataError(RooAbsData::Poisson) );
   RooArgSet result_1q ( fitres_1q->floatParsFinal() );
 
   fitutil->SetNOcentvals();
   fitutil->GetVar("SinsqTh23")->setVal(0.6);
-  RooFitResult *fitres_2q = simPdf.fitTo( data_hists, Save(), Range("fitRangeE"), Range("secondq"), 
+  RooFitResult *fitres_2q = simPdf.chi2FitTo( data_hists, Save(), Range("fitRangeE"), Range("secondq"), 
                                           SplitRange(isRanged), SplitRange(kFALSE), DataError(RooAbsData::Poisson) );
   RooArgSet result_2q ( fitres_2q->floatParsFinal() );
 
@@ -247,8 +247,8 @@ void AsimovFitNO() {
   Double_t fitChi2_1q = fitres_1q->minNll();
   Double_t fitChi2_2q = fitres_2q->minNll();
   Double_t min_chi2;
-  cout << "first q " << TMath::Sqrt( fitChi2_1q ) << endl;
-  cout << "second q" << TMath::Sqrt( fitChi2_2q ) << endl;
+  cout << "first q " << fitChi2_1q << endl;
+  cout << "second q" << fitChi2_2q << endl;
   if (fitChi2_1q == fitChi2_2q) cout << "NOTICE: Minimizer found same minimum for both quadrants." << endl;
   if (fitChi2_1q < fitChi2_2q) { result = &result_1q; min_chi2 = fitChi2_1q; }
   else                         { result = &result_2q; min_chi2 = fitChi2_2q; }
@@ -282,6 +282,24 @@ void AsimovFitNO() {
       fitted.push_back( tracks_fitted );
     }
   }
+
+  // Save histograms of data and fit result
+  TFile fout(s_rootfile, "RECREATE");
+  for (Int_t i = 0; i < N_PID_CLASSES; i++) {
+    TH3D* data_histo;
+    if (pid_map[i] < PID_CUT) data_histo = shower_vector_true[i];
+    else                      data_histo = track_vector_true[i];
+    data_histo->Project3D("yx")->Write(Form("data_hist_%i", i));
+    fitted[i]->Project3D("yx")->Write(Form("fit_hist_%i", i));
+
+    TH3D* difference_histo;
+    difference_histo = HistoDifference(data_histo, fitted[i]); 
+    difference_histo->SetName( Form("diff_histo_%i", i) );
+    difference_histo->Project3D("yx")->Write();
+  }
+  fout.Write();
+  fout.Close();
+
 
   std::vector< std::tuple<TH1*, Double_t, Double_t> > chi2;
   for (Int_t i = 0; i < N_PID_CLASSES; i++) {
