ac5dee2085184b276c8144a2e5834591dd9cdfa6
Lodewijk Nauta <lnauta@outlook.com>
2019-04-26 13:19:27 +0200

    Fix file-exists-check in qsub submit script

--- macros/asimov_fits/qsub/submit_percentage.py
+++ macros/asimov_fits/qsub/submit_percentage.py
@@ -9,5 +9,5 @@ for job in range(0,100):
                   $MONADIR/macros/asimov_fits/qsub/job_{0}.sh".format(job)
 
     files = os.listdir(os.environ["MONADIR"]+"/macros/asimov_fits/output/csv/CrossCheck/percentages/")
-    if not any("{}.csv".format(job) in file for file in files):
+    if not any("_{}.csv".format(job) in file for file in files):
         os.system(qsub_submit)
