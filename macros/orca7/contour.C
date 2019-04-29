ea229138481159afc702e5ab927b734eb5adc17c
Bruno Strandberg <b.strandberg@nikhef.nl>
2019-04-29 20:49:24 +0200

    Further development of the contour script into an application with a fixed set of configurable settings.

--- macros/orca7/contour.C
+++ macros/orca7/contour.C
@@ -68,53 +68,84 @@ int main(const int argc, const char** argv) {
   auto pdfs = o7.fPdfs;
 
   //=====================================================================================
-  // parameter randomisation
+  // create vectors with only oscpars and only systpars
   //=====================================================================================
 
-  vector<TString> oscpars = {"SinsqTh12", "SinsqTh13", "SinsqTh23", "dcp", "Dm21", "Dm31"};
+  vector<RooRealVar*> oscpars = { fu->GetVar("SinsqTh12"), fu->GetVar("SinsqTh13"), fu->GetVar("SinsqTh23"),
+				  fu->GetVar("dcp"), fu->GetVar("Dm21"), fu->GetVar("Dm31") };
+  vector<RooRealVar*> systpars;
 
-  if (RandomisePars) {
+  RooArgSet parset = fu->GetSet();
+  TIterator *it = parset.createIterator();
+  RooRealVar* var;
+
+  while ( ( var = (RooRealVar*)it->Next() ) ) {
+    
+    if ( fu->GetObs().find(var->GetName()) != NULL ) continue; // ignore observables
+
+      Bool_t isOscPar = kFALSE;
+      for (auto o: oscpars) isOscPar = isOscPar || ( o == var );
+      if ( !isOscPar ) systpars.push_back(var);
+      
+  }
+
+  //=====================================================================================
+  // randomise parameters
+  //=====================================================================================
+
+  TRandom  *rand = RooRandom::randomGenerator();
+  rand->SetSeed(seed);
+
+  // randomisation of oscillation parameters
+  if ( RandomisePars ) {
 
     // set to NuFit 4.0 values and limits
     if (InvertedOrdering) o7.Set_NuFit_4p0_IO(fu);
     else o7.Set_NuFit_4p0_NO(fu);
 
-    RooArgSet pars = fu->GetSet();
-    RooArgSet obs  = fu->GetObs();
-    TRandom  *rand = RooRandom::randomGenerator();
-    rand->SetSeed(seed);
+    for (auto var: oscpars) {
+      cout << "NOTICE contour: randomising parameter " << var->GetName() << endl; 
+      Double_t mean  = var->getVal();
+      Double_t sigma = ( var->getMax() - var->getMin() )/3 ;
+      var->setVal( rand->Gaus( mean, sigma ) );
+    }
+    
+  }
 
-    TIterator *it = pars.createIterator();
-    RooRealVar* var;
-    while ( ( var = (RooRealVar*)it->Next() ) ) {
-      
-      if ( obs.find(var->GetName()) != NULL ) continue; // ignore observables
+  // randomisation of systematics, store also the default values
+  std::map<RooRealVar*, Double_t> default_systs;
 
-      Bool_t isOscPar = kFALSE;
-      for (auto o: oscpars) isOscPar = isOscPar || ( o == (TString)var->GetName() );
+  for (auto var: systpars) {
 
-      if ( isOscPar ) {
+    default_systs.insert( std::make_pair( var, var->getVal() ) );
+    
+    if ( !IncludeSystematics ) {
+      cout << "NOTICE contour: fixing parameter " << var->GetName() << endl;
+      var->setConstant(kTRUE);
+    }
+    else  {
+
+      if ( RandomisePars ) {
+	cout << "NOTICE contour: randomising parameter " << var->GetName() << endl; 
 	Double_t mean  = var->getVal();
-	Double_t sigma = ( var->getMax() - var->getMin() )/3 ;
-	var->setVal( rand->Gaus( mean, sigma ) );
-      }
-      else {
-
-	Double_t mean  = var->getVal();
-	Double_t sigma = 0.2;
+	Double_t sigma = 0.15;
 	var->setVal( rand->Gaus(mean, sigma) );
 
       }
-
     }
-
+    
   }
 
-  //=====================================================================================
-  // parameter randomisation
-  //=====================================================================================
+  // theta-23 is set to the input value
+  fu->GetVar("SinsqTh23")->setVal( sinsqth23 );
 
-  // create expectation value data
+  // save the parameter values at which data is created
+  RooArgSet *pars = (RooArgSet*)fu->GetSet().snapshot(kTRUE);
+  
+  //=====================================================================================
+  // create data
+  //=====================================================================================
+  
   std::map< string, TH1* > exps; // map with response name <--> expectation value histogram
 
   for (auto P: pdfs) {
@@ -124,37 +155,34 @@ int main(const int argc, const char** argv) {
     exp->SetNameTitle( hname, hname );
     exps.insert( std::make_pair( (string)P.first, (TH1*)exp ) );
   }
-
-  // save the parameter values
-  RooArgSet *pars = (RooArgSet*)fu->GetSet().snapshot(kTRUE);
   
   //=====================================================================================
-  // set oscillation parameters back to central values, fix osc pars except for tm23, dm31; set
-  // the systematics back to central values
+  // set parameter values for fit start
   //=====================================================================================
-  fu->SetNOcentvals();
+
+  // for fit start, set the osc parameters to NuFit central values
+  if (InvertedOrdering) { o7.Set_NuFit_4p0_IO(fu); }
+  else                  { o7.Set_NuFit_4p0_NO(fu); }
+
+  fu->FreeParLims(); // release the limits
+  
+  // theta-23 is set to the input value, so that the fit starts in the right quadrant
+  fu->GetVar("SinsqTh23")->setVal( sinsqth23 );
+
+  // systematics are set to default values
+  for (auto var: systpars) {
+    var->setVal( default_systs[var] );
+  }
+  
+  // always fix these osc pars, no sensitivity
   fu->GetVar("SinsqTh12")->setConstant(kTRUE);
   fu->GetVar("SinsqTh13")->setConstant(kTRUE);
   fu->GetVar("dcp")->setConstant(kTRUE);
   fu->GetVar("Dm21")->setConstant(kTRUE);
 
-  // if systematics are not fixed set them to starting values for the fit
-  if ( !fixSystematics ) {
-
-    fu->GetVar("E_tilt")     ->setVal( 0. );
-    fu->GetVar("ct_tilt")    ->setVal( 0. );
-    fu->GetVar("skew_mu_amu")->setVal( 0. );
-    fu->GetVar("skew_e_ae")  ->setVal( 0. );
-    fu->GetVar("skew_mu_e")  ->setVal( 0. );
-    fu->GetVar("NC_norm")    ->setVal( 1. );
-    fu->GetVar("Tau_norm")   ->setVal( 1. );
-    fu->GetVar("E_scale")    ->setVal( 0. );
-
-  }
-
-  // debugging
-  fu->GetVar("Tau_norm")->setConstant( kTRUE );
-  fu->GetVar("E_scale") ->setConstant( kTRUE );
+  // for dev, I currently fix tau and E_scale
+  // fu->GetVar("Tau_norm")->setConstant( kTRUE );
+  // fu->GetVar("E_scale") ->setConstant( kTRUE );
 
   //=====================================================================================
   // configure simultaneous fitting
@@ -172,8 +200,12 @@ int main(const int argc, const char** argv) {
   // create likelihood scanners - individual for each PID range and simultaneous
   //=====================================================================================
 
-  RooPlot* frame = fu->GetVar("SinsqTh23")->frame( Range(0.25, 0.75), Title("-log(L) scan vs sinsqth23") );
-
+  RooRealVar* V = fu->GetVar("SinsqTh23");
+  RooPlot frame( *V, 0.3, 0.7, 40 );
+  frame.SetNameTitle( "-log(L) scan in sin^{2}#theta_{23}", "-log(L) scan in sin^{2}#theta_{23}" );
+  frame.GetXaxis()->SetTitle("sin^{2}#theta_{23}");
+  frame.GetYaxis()->SetTitle("-log(L)");
+  
   vector< RooDataHist* > rfdata;         // vector to store pointers to RooFit data, for clean-up
   std::map< TString, RooAbsReal* > nlls; // map with response name <--> nll variable
   for (auto &P: pdfs) {
@@ -184,27 +216,27 @@ int main(const int argc, const char** argv) {
     TString hname = TString("rf_") + (TString)hin->GetName();
     RooDataHist *rfh = new RooDataHist( hname, hname, fu->GetObs(), Import(*hin) );
 
-    RooAbsReal *nll = pdf->createNLL( *rfh, ExternalConstraints( o7.fPriors ), NumCPU(ncpu) );
+    
+    RooAbsReal *nll;
+    if (IncludePriors) { nll = pdf->createNLL( *rfh, ExternalConstraints( o7.fPriors ), NumCPU(ncpu) ); }
+    else { nll = pdf->createNLL( *rfh, NumCPU(ncpu) ); }
 
     nlls.insert( std::make_pair( P.first, nll )  );
     rfdata.push_back( rfh );
   }
 
   // create a LLH scanner from simultaneous fitting, add to map
-  RooAbsReal *nll_sim = simPdf.createNLL( comb, ExternalConstraints( o7.fPriors ), NumCPU(ncpu) );
+  RooAbsReal *nll_sim;
+  if (IncludePriors) { nll_sim = simPdf.createNLL( comb, ExternalConstraints( o7.fPriors ), NumCPU(ncpu) ); }
+  else { nll_sim = simPdf.createNLL( comb, NumCPU(ncpu) ); }
+  
   nlls.insert( std::make_pair("sim", nll_sim) );
 
   //=====================================================================================
   // Minimize and create contour plot
   //=====================================================================================
 
-  if ( nlls.find(channel) == nlls.end() ) {
-    cout << "Supported channels: " << endl;
-    for (auto kv: nlls) cout << kv.first << endl;
-    throw std::invalid_argument("ERROR! contours_wsyst unknown channel " + (string)channel);
-  }
-
-  RooMinimizer *min = new RooMinimizer( *nlls[channel] );
+  RooMinimizer *min = new RooMinimizer( *nll_sim );
 
   // call the minimiser and save the result
   min->hesse();
@@ -240,7 +272,7 @@ int main(const int argc, const char** argv) {
   // plot all LLH curves
   Int_t i = 0;
   for (auto N: nlls) {
-    N.second->plotOn( frame, LineColor(1+i), ShiftToZero(), LineStyle(1+i), Name( N.first ) );
+    N.second->plotOn( &frame, LineColor(1+i), ShiftToZero(), LineStyle(1+i), Name( N.first ) );
     cout << "================================================================================" << endl;
     cout << "NOTICE contours_wsyst finished scanning channel " << N.first << endl;
     cout << "================================================================================" << endl;
@@ -248,17 +280,12 @@ int main(const int argc, const char** argv) {
   }
 
   // add a legend
-  TLegend *leg1 = new TLegend(0.3, 0.6, 0.7, 0.9);
+  TLegend leg1(0.3, 0.6, 0.7, 0.9);
   for (auto N: nlls) {
-    leg1->AddEntry( frame->findObject( N.first ), N.first, "l" );
+    leg1.AddEntry( frame.findObject( N.first ), N.first, "l" );
   }
-  leg1->SetLineWidth(0);
-  leg1->SetFillStyle(0);
-
-  // draw
-  TCanvas *c1 = new TCanvas("c1","c1",1);
-  frame->Draw();
-  leg1->Draw();
+  leg1.SetLineWidth(0);
+  leg1.SetFillStyle(0);
 
   // clone the graphs from RooPlot for easier draw option manipulation
   TGraph *g_cont = (TGraph*)contplot->getObject(1)->Clone("g_cont");
@@ -270,39 +297,33 @@ int main(const int argc, const char** argv) {
   g_cont->GetXaxis()->SetTitle("sin^2#theta_{23}");
   g_cont->GetYaxis()->SetTitle("#Delta m_{31}^2");
   g_cont->GetYaxis()->SetRangeUser(0.5*1e-3, 4.5*1e-3);
+  g_cont->GetXaxis()->SetRangeUser(0., 1.);
 
-  TLegend *leg2 = new TLegend(0.6, 0.6, 0.9, 0.9);
-  leg2->AddEntry(g_cont, "2#sigma contour, sim fit", "l");
-  leg2->SetLineWidth(0);
-  leg2->SetFillStyle(0);
-
-  TCanvas *c2 = new TCanvas("c2","c2",1);
-  c2->SetTicks();
-  g_cont->Draw("AL");
-  leg2->Draw();
+  TLegend leg2(0.6, 0.6, 0.9, 0.9);
+  leg2.AddEntry(g_cont, "2#sigma contour, sim fit", "l");
+  leg2.SetLineWidth(0);
+  leg2.SetFillStyle(0);
 
   //------------------------------------------------------------
   // write the plots to output for further manipulation
   //------------------------------------------------------------
-  TFile fout(outname,"RECREATE");
+
+  TFile fout((TString)outfile, "RECREATE");
   g_cont->Write("contour_2sigma");
-  leg2  ->Write("legend_contour_2sigma");
-  frame ->Write("llhscan");
-  leg1  ->Write("legend_llhscan");
+  leg2   .Write("legend_contour_2sigma");
+  frame  .Write("llhscan");
+  leg1   .Write("legend_llhscan");
   pars  ->Write("data_parameters");  
   result->Write("fitresult");
   for (auto e: exps) e.second->Write((TString)e.first);
   fout.Close();
 
+  //------------------------------------------------------------
+  // clean-up
+  //------------------------------------------------------------
+  for (auto d: rfdata) if (d) delete d;
+  if (min) delete min;
+    
   cout << "Total run time: " << (Double_t)timer.RealTime() << endl;
-
-  // remove junk from compilation
-  system("rm contours_wsyst_C*");
-
-}
-
-int main() {
-
-  contours_wsyst();
 
 }
