a6adc369c6787702a98f9826452c8ae8c3bd8e41
Lodewijk Nauta <lnauta@outlook.com>
2019-05-06 16:39:51 +0200

    Add qsub compiled code callers

---
+++ macros/asimov_fits/sensitivity_chi2_inf/AsimovQSubHeader.h
@@ -0,0 +1,4 @@
+#include "Rtypes.h"
+
+void AsimovFitNBinsNOTh23Range_PercentageOfMC(Int_t jobnumber=0, Int_t N_PID=3);
+void AsimovFitNBinsIOTh23Range_PercentageOfMC(Int_t jobnumber=0, Int_t N_PID=3);
