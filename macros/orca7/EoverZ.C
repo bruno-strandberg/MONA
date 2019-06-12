3771d52650bd3f8ec0180f550c95366814021b7d
Bruno Strandberg <b.strandberg@nikhef.nl>
2019-06-12 16:18:08 +0200

    Improved the EoverZ macro, slightly.

--- macros/orca7/EoverZ.C
+++ macros/orca7/EoverZ.C
@@ -1,5 +1,10 @@
 #include "ORCA7.C"
 #include "NMHUtils.h"
+#include "FitUtilWsyst.h"
+#include "FitPDF.h"
+#include "TH1.h"
+#include "TStyle.h"
+#include "TLegend.h"
 
 void EoverZ() {
   
@@ -7,7 +12,17 @@ void EoverZ() {
   FitUtilWsyst *fu = o7.fFitUtil;
   FitPDF *trkpdf = o7.fPdfs["trk"];
 
-  // find the bin limits
+  //---------------------------------------------------------------------
+  // set to NuFit3.2, free the limits
+  //---------------------------------------------------------------------
+
+  o7.Set_NuFit_3p2_NO();
+  fu->FreeParLims();
+
+  //---------------------------------------------------------------------
+  // define binning for the E/cosz plot
+  //---------------------------------------------------------------------
+
   TH3D* ht = o7.fResps["trk"]->GetHist3DReco();
 
   Double_t emin  = ht->GetXaxis()->GetBinCenter( ht->GetXaxis()->FindBin( o7.f_F_emin ) );
@@ -21,21 +36,22 @@ void EoverZ() {
   // template histogram
   auto bins = NMHUtils::GetLogBins(20, xmin, xmax);
   TH1D* hTemplate = new TH1D("template","template", 20, &bins[0]);
-  vector< TH1D* > hists, hists_norm;
+  vector< std::pair<TString,TH1D*> > hists;
 
-  for (Double_t th23 = 0; th23 <= 1; th23 += 0.05) {
+  //---------------------------------------------------------------------
+  // scan in theta-23
+  //---------------------------------------------------------------------
+  
+  for (Double_t th23 = 0; th23 <= 1.0; th23 += 0.25) {
 
     fu->GetVar("SinsqTh23")->setVal( th23 );
     TH3D *htrk = trkpdf->GetExpValHist();
 
-    TString hname      = "hist_th23_" + (TString)to_string(th23);
-    TString hname_norm = "hist_th23_norm_" + (TString)to_string(th23);
-
+    TString htitle = "Model, sin^{2}#theta_{23}=0." + to_string( (Int_t)(th23*100) );
+    if (th23 == 1.0) htitle = "Model, sin^{2}#theta_{23}=1.0";
+    TString hname  = "hist_th23_" + (TString)to_string(th23);
     TH1D* h = (TH1D*)hTemplate->Clone( hname );
-    h->SetNameTitle(hname, hname);
-
-    TH1D* h_norm = (TH1D*)hTemplate->Clone( hname_norm );
-    h_norm->SetNameTitle(hname_norm, hname_norm);
+    h->SetNameTitle(hname, htitle);
 
     for (Int_t xbin = 1; xbin <= htrk->GetXaxis()->GetNbins(); xbin++) {
       for (Int_t ybin = 1; ybin <= htrk->GetYaxis()->GetNbins(); ybin++) {
@@ -51,46 +67,62 @@ void EoverZ() {
 
 	Int_t xbin = h->GetXaxis()->FindBin( -E/ct );
 	h->AddBinContent( xbin, bc );
-	h_norm->AddBinContent( xbin, bc/htrk->Integral() );
 
       }
     }
 
     delete htrk;
-    hists.push_back( h );
-    hists_norm.push_back( h_norm );
+    hists.push_back( std::make_pair(htitle, h) );
 
   }
 
   gStyle->SetOptStat(0);
 
   TCanvas *c1 = new TCanvas("c1","c1",1);
+  c1->SetTicks();
+  c1->GetPad(0)->SetLeftMargin(0.12);
+  c1->GetPad(0)->SetBottomMargin(0.12);
 
-  Int_t counter = 1;
+  hists[0].second->SetTitle("-E/cosz dependence on sin^{2}#theta_{23}");
 
-  hists[0]->GetXaxis()->SetTitle("-E_{reco}/cosz_{reco}");
-  hists[0]->GetYaxis()->SetTitle("N_{expected}");
-  hists[0]->SetTitle("Scan in sin^{2}#theta_{23}");
-  hists[0]->GetXaxis()->SetRangeUser(5,500);
+  hists[0].second->GetXaxis()->SetRangeUser(5,500);
+  
+  hists[0].second->GetXaxis()->SetTitle("-E_{reco}/cosz_{reco}");
+  hists[0].second->GetYaxis()->SetTitle("N_{expected}");
+
+  hists[0].second->GetXaxis()->SetTitleSize(0.05);
+  hists[0].second->GetYaxis()->SetTitleSize(0.05);
+  hists[0].second->GetXaxis()->SetLabelSize(0.04);
+  hists[0].second->GetYaxis()->SetLabelSize(0.04);
+  hists[0].second->GetXaxis()->SetTitleOffset(0.95);
+  hists[0].second->GetYaxis()->SetTitleOffset(0.95);
+  hists[0].second->GetXaxis()->CenterTitle();
+  hists[0].second->GetYaxis()->CenterTitle();
+
+  TLegend *leg = new TLegend(0.5, 0.5, 0.88, 0.88);
+  leg->SetFillStyle(0);
+  leg->SetLineColor(0);
+  
+  Int_t counter = 2;
 
   for (auto g: hists) {
-    g->SetLineWidth(2);
-    g->SetLineColor(counter++);
-    g->Draw("same");
+
+    g.second->SetLineWidth(2);
+    g.second->SetLineColor(counter++);
+
+    if ( g.first == "Model, sin^{2}#theta_{23}=0." + to_string( (Int_t)(0.5*100) ) ) {
+      g.second->SetMarkerStyle(21);
+      g.second->SetLineColor(kBlack);
+      g.second->Draw("Esame");
+      leg->AddEntry(g.second, "Pseudo-data at sin^{2}#theta_{23}=0.5", "lep");
+    }
+    else {
+      leg->AddEntry(g.second, g.first, "l");
+    }
+    
+    g.second->Draw("HISTsame");
   }
 
-  TCanvas *c2 = new TCanvas("c2","c2",1);
-
-  hists_norm[0]->GetXaxis()->SetTitle("-E_{reco}/cosz_{reco}");
-  hists_norm[0]->GetYaxis()->SetTitle("Event density");
-  hists_norm[0]->SetTitle("Scan in sin^{2}#theta_{23}");
-  hists_norm[0]->GetXaxis()->SetRangeUser(5,500);
-
-  counter = 1;
-  for (auto g: hists_norm) {
-    g->SetLineWidth(2);
-    g->SetLineColor(counter++);
-    g->Draw("same");
-  }
-
+  leg->Draw();
+  
 }
